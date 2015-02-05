ccb1071f63836b824f9ef7146e217fff458dde1f
ghadirinia <ghadirinia.marzieh@gmail.com>
2015-02-05 11:34:52 +0330

    Initial commit

---
+++ src/main/java/user/Welcome.java
@@ -0,0 +1,23 @@
+package user;
+
+/**
+ * Created by DotinSchool2 on 2/2/2015.
+ */
+public class Welcome {
+
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // all struts logic here
+    public String execute() {
+
+        return "SUCCESS";
+    }
+}
